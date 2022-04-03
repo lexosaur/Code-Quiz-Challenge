0023be92deb75d9a1ba1a634d2e60abbf77d2ac5
Alexis Garcia <alexis.garciad@gmail.com>
2022-04-02 21:12:09 -0600

    final changes

---
+++ docs/assets/questions.js
@@ -0,0 +1,46 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include",
+      multiChoice: ["1. Strings",
+      "2. Booleans",
+      "3. Alerts",
+      "4. Numbers"],
+      answer: "3. Alerts"
+    },
+  
+    {
+      title: "The condition in an if / else statement is enclosed within ______.",
+      multiChoice: ["1. Quotes",
+      "2. Curly brackets",
+      "3. Parentheses",
+      "4. Square brackets"],
+      answer: "3. Parentheses"
+    },
+  
+    {
+      title: "Arrays in JavaScript can be used to store _________.",
+      multiChoice: ["1. Numbers and strings",
+      "2. Other arrays",
+      "3. Booleans",
+      "4. All of the above"],
+      answer: "2. Other arrays"
+    },
+  
+    {
+      title: "String values must be enclosed within _________ when being assigned to variables.",
+      multiChoice: ["1. Commas",
+        "2. Curly brackets",
+        "3. Quotes",
+        "4. Parentheses"],
+      answer: "4. Parentheses"
+    },
+  
+    {
+      title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      multiChoice: ["1. JavaScript",
+        "2. Terminal/bash",
+        "3. For loops",
+        "4. console.log"],
+      answer: "1. JavaScript"
+    }
+  ];
